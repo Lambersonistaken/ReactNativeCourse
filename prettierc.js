993ad5584ed3d89e4aef90c1f9927c97bb3b9e47
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2023-10-18 15:37:51 +0300

    eslint prettier

---
+++ prettierc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "none",
+  arrowParens: "avoid",
+  semi: false,
+};
