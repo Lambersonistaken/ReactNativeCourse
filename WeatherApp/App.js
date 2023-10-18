2e2ce8448199fec7a36d2f8bfdbb4474dfc9aced
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2023-10-18 16:10:56 +0300

    safeareaview

--- WeatherApp/App.js
+++ WeatherApp/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView } from "react-native";
 
 function App() {
   return (
-    <View>
-      <Text>Weather App</Text>
-    </View>
+    <SafeAreaView>
+      <View>
+        <Text>Weather App</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
