6bfab3440a59163456120cbbfaa7896ce408ffb0
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2023-10-18 15:44:47 +0300

    first app

--- prettierc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  bracketSpacing: true,
-  jsxBracketSameLine: false,
-  singleQuote: true,
-  tabWidth: 2,
-  trailingComma: "none",
-  arrowParens: "avoid",
-  semi: false,
-};
