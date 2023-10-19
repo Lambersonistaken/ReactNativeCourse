ac21061b72ffb19ed47e8bb9b3c9323f859bba44
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2023-10-20 02:17:00 +0300

    update

--- WeatherApp/App.js
+++ WeatherApp/App.js
@@ -1,78 +1,19 @@
-import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
-import { Fontisto } from "@expo/vector-icons";
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import CurrentWeather from "./src/components/CurrentWeather";
 
-export default function App() {
+function App() {
   return (
-    <SafeAreaView style={styles.wrapper}>
-      <View style={styles.container}>
-        <Text style={styles.title}>Current Weather</Text>
-        <Text style={styles.subtext}>6</Text>
-        <Text style={styles.subtext}>Feels like 5</Text>
-
-        <View style={styles.highLowWrapper}>
-          <Text style={styles.subtext}>High: 8</Text>
-          <Text style={styles.subtext}>Low: 6</Text>
-        </View>
-      </View>
-      <View style={styles.bodyWrapper}>
-        <Fontisto name="cloudy-gusts" size={100} color="black" />
-        <Text style={styles.subtext2}>Cloudy</Text>
-        <Text style={styles.subtext2}>Wind: 10km/h</Text>
-      </View>
-
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    <View style={styles.container}>
+      <CurrentWeather />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 24,
-    textAlign: "center",
-  },
-  wrapper: {
-    backgroundColor: "#eadee0",
-    flex: 1,
-    padding: 24,
-  },
-  title: {
-    marginTop: 16,
-    paddingVertical: 8,
-    paddingHorizontal: 16,
-    borderWidth: 4,
-    borderColor: "#20232a",
-    borderRadius: 6,
-    backgroundColor: "#61dafb",
-    color: "#20232a",
-    textAlign: "center",
-    fontSize: 30,
-    fontWeight: "bold",
-  },
-  subtext: {
-    textAlign: "center",
-    marginTop: 16,
-    fontSize: 36,
-    color: "orange",
-  },
-  subtext2: {
-    textAlign: "center",
-    marginTop: 16,
-    fontSize: 24,
-    color: "black",
-  },
-  highLowWrapper: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-  },
-  bodyWrapper: {
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#eadee0",
-    borderWidth: 4,
-    borderColor: "#20232a",
-    borderRadius: 6,
-    padding: 24,
   },
 });
+
+export default App;
