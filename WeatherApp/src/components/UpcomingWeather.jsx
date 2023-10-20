4cc95d38a3b21badee09e067b648184814db2d6b
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2023-10-21 00:46:57 +0300

    new component created

---
+++ WeatherApp/src/components/UpcomingWeather.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { SafeAreaView, View } from "react-native";
+
+function UpcomingWeather() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Upcoming Weather</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default UpcomingWeather;
