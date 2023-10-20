b0776114633781e7087c5b6b4e3004cf1660b97f
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2023-10-21 01:00:28 +0300

    component rendered

--- WeatherApp/src/components/UpcomingWeather.jsx
+++ WeatherApp/src/components/UpcomingWeather.jsx
@@ -1,14 +1,18 @@
 import React from "react";
-import { SafeAreaView, View } from "react-native";
+import { SafeAreaView, View, StyleSheet, Text } from "react-native";
 
 function UpcomingWeather() {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Upcoming Weather</Text>
-      </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Upcoming Weather</Text>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default UpcomingWeather;
