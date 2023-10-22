b09dcdcf442ba4072aeab6173bf51eac9459bfc3
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2023-10-22 17:54:35 +0300

    list added

--- WeatherApp/src/components/UpcomingWeather.jsx
+++ WeatherApp/src/components/UpcomingWeather.jsx
@@ -1,10 +1,73 @@
 import React from "react";
-import { SafeAreaView, View, StyleSheet, Text } from "react-native";
+import { SafeAreaView, View, StyleSheet, Text, FlatList } from "react-native";
+import { Feather } from "@expo/vector-icons";
+
+const DATA = [
+  {
+    dt_txt: "2021-07-27 12:00:00",
+    main: {
+      temp_max: 30,
+      temp_min: 25,
+    },
+    weather: [
+      {
+        main: "Clouds",
+      },
+    ],
+  },
+  {
+    dt_txt: "2021-07-28 15:00:00",
+    main: {
+      temp_max: 20,
+      temp_min: 15,
+    },
+    weather: [
+      {
+        main: "Rain",
+      },
+    ],
+  },
+  {
+    dt_txt: "2021-07-29 18:00:00",
+    main: {
+      temp_max: 25,
+      temp_min: 20,
+    },
+    weather: [
+      {
+        main: "Rain",
+      },
+    ],
+  },
+];
+
+const Item = (props) => {
+  const { dt_txt, min, max, condition } = props;
+  return (
+    <View>
+      <Feather name="sun" size={50} color="white" />
+      <Text>{dt_txt}</Text>
+      <Text>{min}</Text>
+      <Text>{max}</Text>
+    </View>
+  );
+};
 
 function UpcomingWeather() {
+  const renderItem = ({ item }) => {
+    return (
+      <Item
+        dt_txt={item.dt_txt}
+        min={item.main.temp_min}
+        max={item.main.temp_max}
+        condition={item.weather[0].main}
+      />
+    );
+  };
   return (
     <SafeAreaView style={styles.container}>
       <Text>Upcoming Weather</Text>
+      <FlatList data={DATA} renderItem={renderItem}></FlatList>
     </SafeAreaView>
   );
 }
