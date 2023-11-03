9af5b2f3fa54e70e7f6c76a3720de7593e048eb8
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2023-11-03 17:12:30 +0300

    refactoring city screen

--- WeatherApp/src/components/IconText.jsx
+++ WeatherApp/src/components/IconText.jsx
@@ -2,10 +2,22 @@ import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { Feather } from "@expo/vector-icons";
 
-function IconText() {
-  return <View></View>;
+function IconText({ iconName, text, iconColor }) {
+  return (
+    <View>
+      <Feather name={iconName} size={50} color={iconColor} />
+      <Text style={styles.textTheme}>{text}</Text>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  textTheme: {
+    color: "white",
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
 
 export default IconText;
