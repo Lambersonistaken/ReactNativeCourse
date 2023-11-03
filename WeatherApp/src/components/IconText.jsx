8588e43384fb00813732bc3af9193aae6156053b
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2023-11-03 17:06:11 +0300

    new component created

---
+++ WeatherApp/src/components/IconText.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { Feather } from "@expo/vector-icons";
+
+function IconText() {
+  return <View></View>;
+}
+
+const styles = StyleSheet.create({});
+
+export default IconText;
