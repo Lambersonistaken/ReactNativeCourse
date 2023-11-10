56ba99a58258bbff2405e25dd05b5743cba00b1b
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2023-11-10 16:27:44 +0300

    update

--- WeatherApp/App.js
+++ WeatherApp/App.js
@@ -4,13 +4,18 @@ import UpcomingWeather from "./src/screens/UpcomingWeather";
 import City from "./src/screens/City";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
+import { Feather } from "@expo/vector-icons";
 const Tab = createBottomTabNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarActiveTintColor: "tomato",
+          tabBarInactiveTintColor: "gray",
+        }}
+      >
         <Tab.Screen name={"Current"} component={CurrentWeather} />
         <Tab.Screen name={"Upcoming"} component={UpcomingWeather} />
         <Tab.Screen name={"City"} component={City} />
